2eaa43e81aa9be541d4e3c9cbc8067629f3b8c82
DiFi Era <83540733+jdefi@users.noreply.github.com>
2024-03-28 04:54:54 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { SwanSaturnTestnet } from "@thirdweb-dev/chains";
 import "./styles/globals.css";
 import { Toaster } from "./components/ui/Toaster";
 import { getGasless } from "./utils/getGasless";
